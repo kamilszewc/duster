0a6ebf8ef15e0eae40769f27cb06f4147d2829a0
Dr. Kamil Szewc <kamil.szewc@essteyr.com>
2020-12-30 13:26:01 +0100

    Old webapp removed

--- webapp/config.py
+++
@@ -1,9 +0,0 @@
-
-class Config(object):
-    # General Flask
-    DEBUG = False
-    TESTING = False
-    PORT = 8080
-
-    # Database
-    DATABASE = 'sqlite:///../db/concentration.sqlite'  # Database engine and name
